7586708051671896a4c28733e68323994aaf846b
jaessss <43599718+jaessss@users.noreply.github.com>
2018-10-09 20:23:48 +0900

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,22 @@
+//18.##.## #No. <Title>
+//Coments : 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+int ans;
+
+int main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL);
+	setbuf(stdout, NULL);
+
+	
+
+	cout << ans << endl;
+	
+	return 0;
+}
